65b27860ad61cf45d59a896dabec4a80ed76a29a
Mayank Nagpal <43073963+mayankn05@users.noreply.github.com>
2021-09-14 16:37:35 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react';
+import Panda from "./panda1.jpeg"
+const App = () => {
+  return(
+    <>
+    <div className="page">
+    <img src= {Panda} alt="ilovePanda"/>
+      <div className= "text">
+    <h1>Wake up beauty. It's time to beast...</h1>
+    <h2>Join Us</h2>
+    <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quasi odit ipsa omnis fugiat voluptatem temporibus cupiditate porro nisi, maxime sapiente perferendis possimus accusantium necessitatibus laboriosam fugit odio nesciunt! Voluptas!
+    </p>
     </div>
-  );
+    </div>
+    </>
+  )
 }
-
-export default App;
+export default App
